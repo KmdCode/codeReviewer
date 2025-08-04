8ba19fc53315e162a0bd6f747af4f986312e8bf8
karabomodise260@gmail.com <karabomodise260@gmail.com>
2025-08-04 16:14:59 +0200

    Github Integration test

--- frontend/src/app/api/ai-review/route.ts
+++ frontend/src/app/api/ai-review/route.ts
@@ -1,8 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { GoogleGenerativeAI } from '@google/generative-ai';
+import { getAiReview } from '@/utils/reviews/ai-review';
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
-console.log(process.env.GEMINI_API_KEY);
 
 export const POST = async (req: NextRequest) => {
 
@@ -13,29 +11,10 @@ export const POST = async (req: NextRequest) => {
       return NextResponse.json({ error: 'Missing code or language' }, { status: 400 });
     }
 
-    const prompt = `
-You are an expert in ${language}. Perform a code review on the following code and return a JSON array of issues:
-[
-  { "line": 1, "message": "Example issue message" },
-  ...
-]
-
-Code:
-${code}
-    `;
-
-    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-pro' });
-
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const text = response.text();
-
-    const start = text.indexOf('[');
-    const end = text.lastIndexOf(']');
-    const jsonString = text.slice(start, end + 1);
-    const violations = JSON.parse(jsonString);
+    const violations = await getAiReview(code, language);
 
     return NextResponse.json({ violations });
+    
   } catch (error) {
     console.error('Gemini AI Review Error:', error);
     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
---
+++ frontend/src/utils/reviews/ai-review.ts
@@ -0,0 +1,32 @@
+import { GoogleGenerativeAI } from '@google/generative-ai';
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+console.log(process.env.GEMINI_API_KEY);
+
+export const getAiReview = async (code: string, language: string) => {
+
+
+    const prompt = `
+You are an expert in ${language}. Perform a code review on the following code and return a JSON array of issues:
+[
+  { "line": 1, "message": "Example issue message" },
+  ...
+]
+
+Code:
+${code}
+    `;
+
+    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-pro' });
+
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+
+    const start = text.indexOf('[');
+    const end = text.lastIndexOf(']');
+    const jsonString = text.slice(start, end + 1);
+    // const violations = JSON.parse(jsonString);
+
+    return JSON.parse(jsonString);
+}
